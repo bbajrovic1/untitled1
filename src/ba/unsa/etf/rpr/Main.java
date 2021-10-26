bfe481d78aad8610fd324fa85c132d50c1e2613e
Bakir Bajrović <bbajrovic1@etf.unsa.ba>
2021-10-26 22:43:46 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ba/unsa/etf/rpr/Main.java

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,6 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("H657896789668");
+        System.out.println("H65789668");
     }
 }
